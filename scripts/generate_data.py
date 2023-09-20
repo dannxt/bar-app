7f4222cfaf34bcd165ce904ea12c9fdcf43868ea
dan <danshen@berkeley.edu>
2023-09-21 00:47:58 +0800

    Add new build details

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -2,6 +2,7 @@ import os
 import math
 import random
 
+file = '3a'
 numOfParts = 12
 DEFAULT_DATA_LENGTH = (22 * 2 ** 22 * 3) / numOfParts
 dataLength = DEFAULT_DATA_LENGTH
@@ -28,7 +29,7 @@ for routeNumber in listOfRouteNames:
         start_time = os.times()[4]
         myString = generateDataString(routeNumber, dataLength)
         try:
-            with open(f'../len-22-333-data/routeData{routeNumber}-{j}.js', 'w') as f:
+            with open(f'../len-22-333-{file}-data/routeData{routeNumber}-{j}.js', 'w') as f:
                 f.write(f"module.exports = {repr(myString)};")
             end_time = os.times()[4]
             elapsed_time = end_time - start_time
